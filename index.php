271adb56fc7c33cefc91ce11a44f1b0417624b23
kidsherman <kidsherman123@163.com>
2015-05-12 14:10:48 +0800

    Changes to be committed: 	modified:   index.php
     Untracked files:
    	responseText.func.inc.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ class wechat
         }
     }
 
-    public function responseMsg()
+public function responseMsg()
     {
 		//get post data, May be due to the different environments
 		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
@@ -43,22 +43,57 @@ class wechat
 		if (!empty($postStr)){
                 
               	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $keyword = trim($postObj->Content);
+				$RX_TYPE = trim($postObj->MsgType);
 
-				if(!empty( $keyword ))
-                {
-                	$contentStr = "感谢你的回复！老臭虫正在调试微信API接口，所以回复的信息对您来说暂时没有什么意义，您就当帮我测试了：）";
-                	//$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	$resultStr = _response_text($postObj,$contentStr);
-					echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
+				switch($RX_TYPE)
+				{
+					case "text":
+						$resultStr = $this->handleText($postObj);
+						break;
+					case "event":
+						$resultStr = $this->handleEvent($postObj);
+						break;
+					default:
+						$resultStr = "Unknow msg type: ".$RX_TYPE;
+						break;
+				}
+				echo $resultStr;
+		}else{
+			echo "";
+			exit;
+		}
+	}
 
-        }else {
-        	echo "";
-        	exit;
+	public function handleText($postObj)
+    {
+        $keyword = trim($postObj->Content);
+
+        if(!empty( $keyword ))
+        {
+            $contentStr = "感谢你的回复！老臭虫正在调试微信API接口，所以回复的信息对您来说暂时没有什么意义，您就当帮我测试了：）";
+            //$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			$resultStr = _response_text($postObj,$contentStr);
+            echo $resultStr;
+        }else{
+            echo "Input something...";
         }
+    }
+    
+   
+     public function handleEvent($object)
+    {
+        $contentStr = "";
+        switch ($object->Event)
+        {
+            case "subscribe":
+                $contentStr = "感谢您关注老臭虫"."\n"."-----------------------------"."\n"."请君看取百年事,业就扁舟泛五湖";
+                break;
+            default :
+                $contentStr = "Unknow Event: ".$object->Event;
+                break;
+        }
+        $resultStr = _response_text($object, $contentStr);
+        return $resultStr;
     }
 		
 	private function checkSignature()
